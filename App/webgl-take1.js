e1c671cd58230511802e60e6ad9a20fd223179cf
TooleConor <117325876+TooleConor@users.noreply.github.com>
2025-11-23 13:28:48 -0700

    initializing repo

---
+++ App/webgl-take1.js
@@ -0,0 +1,57 @@
+import { initBuffers } from "../init-buffers.js";
+import { drawScene } from "../draw-scene.js";
+import { initShaderProgram } from "./shader-program.js";
+
+main();
+
+function main() {
+  const canvas = document.querySelector("#gl-canvas");
+  const gl = canvas.getContext("webgl");
+
+  if (gl === null) {
+    alert(
+      "Unable to initialize WebGL. Your browser or machine may not support it."
+    );
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Vertex shader program
+  const vsSource = `
+      attribute vec4 aVertexPosition;
+      uniform mat4 uModelViewMatrix;
+      uniform mat4 uProjectionMatrix;
+      void main() {
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      }
+    `;
+
+  const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(
+        shaderProgram,
+        "uProjectionMatrix"
+      ),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+    },
+  };
+
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+  const buffers = initBuffers(gl);
+  drawScene(gl, programInfo, buffers);
+}
