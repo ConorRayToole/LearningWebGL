bbc00978e48441daa9abf8f199f5fdc8b3d71a01
TooleConor <117325876+TooleConor@users.noreply.github.com>
2025-11-23 17:15:40 -0700

    added color

--- App/shader-fragment.glsl
+++ App/shader-fragment.glsl
@@ -5,15 +5,23 @@ precision lowp float; // prevents us from writing lowp every time
 uniform float iTime;
 varying vec3 vPosition;
 
+vec3 pallete(float t) { //returns gradient of pallete or 4 colors 
+    vec3 a = vec3(0.558, 0.57, .57);
+    vec3 b = vec3(-0.878, 0.395, .395);
+    vec3 c = vec3(1.359, 0.8, 0.8);
+    vec3 d = vec3(-0.682, 0.333, 0.667);
+    return a + b*cos(6.28318*(c*t+d));
+}
+
 void main(void) {
     vec2 uv = vec2(vPosition.x, vPosition.y);
-    vec3 ringColor = vec3(0.0, 0.5, 1.0);
-
     float d = length(uv); // distance from origin
+    vec3 ringColor = pallete(d);
+
     d = sin(d * 8.0 + iTime)/8.0;
     d = abs(d); //makes inside ring white
 
-    d = 0.05/d; 
+    d = 0.03/d; 
     ringColor *= d;
 
     gl_FragColor = vec4(ringColor, 1.0);
